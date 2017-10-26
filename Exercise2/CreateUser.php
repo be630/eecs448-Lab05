f7d2d5ce0e466a808c94020ccd31546259eba08d
Blaine Edmondson <blaine@ku.edu>
2017-10-26 02:40:41 -0500

    Added the post from the html form, and added the connection. I also added two if statements to check if the connection failed, and if the username field was blank.

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -1,13 +1,22 @@
 <?php
 
-
-
-
-
-
-
-
-
-
-
+	$new_username = $_POST["username"];
+	
+	$data_base = new mysqli("mysql.eecs.ku.edu", "bedmondson", 'P@$$word.123', "bedmondson");
+	
+	if($data_base->connect_errno)
+	{
+		printf("Unable to reach server: %s\n", $data_base->connect_error);
+		exit();
+	}
+	
+	if(strlen($new_username) == 0)
+	{
+		echo "Username was blank. Try again.";
+		//add main page exit later.
+		exit();
+	}
+	
+	$check_string = "SELECT user_id FROM Users WHERE user_id='$new_username'";
+	$check_name = $data_base->query($check_string);
 ?>
